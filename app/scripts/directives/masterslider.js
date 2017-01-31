82169d4b109e788403eabda0888b44a50e077bbd
Poul Cummings <poul@cummings.dk>
2017-01-31 22:23:52 +0000

    moved master init to directive

--- app/scripts/directives/masterslider.js
+++ app/scripts/directives/masterslider.js
@@ -6,7 +6,18 @@
         return {
             templateUrl: '/views/directives/masterslider.html',
             controller: 'MainController',
-            replace: true
+            replace: true,
+            link: function(scope, elem, attr) {
+              VolcannoInclude.masterSliderInit("mastersliderFullWidth03");
+              VolcannoInclude.pieChartInit('pieChart-3');
+              // INCLUDE OWL CAROUSEL
+              VolcannoInclude.owlCarouselInit('testimonial-carousel-02');
+              VolcannoInclude.owlCarouselInit('client-carousel');
+              //CONTACT FORM AJAX
+              VolcannoInclude.contactFormAjax('request-quote');
+              //NEWSLETTER FORM AJAX
+              VolcannoInclude.contactFormAjax('newsletter');
+            },
         };
     });
 })();
